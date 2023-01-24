a00759161e929a2fef654f38f931e15c16227cfd
Robin_ <77126392+TrinhRobin@users.noreply.github.com>
2023-01-24 23:43:17 +0100

    Add streamlit+some modifications

---
+++ app_projet.py
@@ -0,0 +1,68 @@
+import streamlit as st 
+from util import *
+import random
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns 
+st.title("Porjects Attribution")
+st.header("Based on mariage lemma ")
+
+uploaded_file = st.file_uploader("please drag and drop the csv file here :",type='csv')
+if uploaded_file is not None:
+    # To read file as bytes:
+    #bytes_data = uploaded_file.getvalue()
+    #st.write(bytes_data)
+
+    # To convert to a string based IO:
+    #stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    #st.write(stringio)
+
+    # To read file as string:
+    #string_data = stringio.read()
+    #st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    st.write("Check that the csv file is well imported:")
+    df = pd.read_csv(uploaded_file)
+    st.write(df.head())
+
+    
+
+    email_list = st.text_input("Do you want to apply a filter based on email adresses?")
+    st.write(email_list )
+    df_clean = cleaning_dataset(df)
+    st.write("The students' names are :")
+    st.write(df_clean[["Nom","Prénom"]].drop_duplicates().reset_index(drop=True))
+    
+
+    st.write("The projects to be allocated are :")
+    unique_projects = np.unique(np.concatenate([df_clean["Choix1"].unique(),
+                                        df_clean["Choix2"].unique(),
+                                        df_clean["Choix3"].unique()]))
+    st.write(unique_projects)
+
+    st.write("Congratulations ! Now let's the matching begins !")
+    places ={proj : 0 for proj in   unique_projects }
+    for proj in unique_projects:
+        places[proj] = st.number_input(f'number of places for Project {proj} ',0,5,1)
+        
+  
+    student_vows = {df_clean.loc[i,"Nom"]+'_'+df_clean.loc[i,"Prénom"]: [df_clean.loc[i,f'Choix{j}'] for j in range(1,4) ] for i in df_clean.index}
+    st.write( "Those are the student' vows:",student_vows)
+    vows_projects=projects_ranking(df_clean,unique_projects)
+    st.write("And on the opposite side, the projects' vows:",vows_projects)
+
+    st.write("The possible places for each project is",places) 
+
+    b =st.button("it's time for matching algorithm !")
+    if b :
+        st.header("Computing...please wait...")
+        attributions = stable_matching_algorithm_unbalanced_class(student_vows,vows_projects,places)
+        st.write("the final attributions are :", attributions)
+        st.write("Is it a good matching ? Analyzing the distribution of student's vows :")
+        fig = plt.figure()
+        sns.histplot(evaluate_satisfaction( student_vows ,attributions))
+        plt.title("Histogram of the ranking of final projects attributions (0 = first vow, 5= last vow ")
+        st.pyplot(fig)
+
---
+++ util.py
@@ -0,0 +1,93 @@
+import random
+import pandas as pd
+import numpy as np
+
+def fill_all_student_projects(list_students_projects,
+                              list_all_students,
+                              method_ranking ):
+    student_queue = sorted([y for y in list_all_students if y not in list_students_projects], key=lambda k: method_ranking(k))
+    return(list_students_projects+student_queue)
+
+def fill_all_projects(student_vows,
+                              list_all_projects,
+                              method_ranking ):
+    project_queue = sorted([y for y in list_all_projects if y not in student_vows], key=lambda k: method_ranking(k))
+    return(student_vows+project_queue)
+    
+def cleaning_dataset(df,list_mails=[]):
+    df_clean =df.drop_duplicates(subset=['Nom','Prénom'],keep='last')
+    df_clean =  df_clean.dropna()
+    df_clean.columns = ['Horodateur', 'Nom', 'Prénom', 'Adresse_Mail','Choix1','Raison_Choix1','Choix2','Raison_Choix2',
+                   'Choix3','Equipe','OS','Memoire','Github','Last']
+    #filtre adresse mail
+    if list_mails: 
+        print("Filtre activé")
+        df_clean=df_clean[df_clean['Adresse_Mail'].isin(list_mails)]
+    return df_clean
+    
+def projects_ranking(df,list_projects, 
+                     method_class1=len,method_class2= lambda x : random.random()):
+   
+    
+    #A mettre en paramètre ? avec webscrapping
+    list_all_students  = set([ x[0] +"_"+x[1] for x in df.loc[:,["Nom","Prénom"]].values])
+    df['score1']=df["Raison_Choix1"].apply(lambda x : method_class1(x))
+    df['score2']=df["Raison_Choix2"].apply(lambda x : method_class1(x))
+    
+    
+    vows_projects = {project :[ x[0]+'_'+x[1] 
+          for x in df.loc[df["Choix1"]==project,].sort_values(by='score1',ascending=False).loc[:,["Nom","Prénom"]].values]+
+                     [ x[0]+'_'+x[1] 
+          for x in df.loc[df["Choix2"]==project,].sort_values(by='score2',ascending=False).loc[:,["Nom","Prénom"]].values] 
+                     for project in  list_projects}
+    return({ project : fill_all_student_projects(vows_projects[project], list_all_students,method_class2) for project in vows_projects})
+    
+def remove_student_from_waiting_list(applicant,waiting_list):
+    """
+    Creating a new list of the waiting_list's keys without the key applicant
+    """
+    return [x for x in waiting_list if x != applicant]
+
+
+def update_matchings(student, choice,matchings,rankings):
+    """
+    Update the allocation of the project choice by adding applicant to it and then sort it according to
+    the project's vows (contained in rankings)
+    """
+    matchings[choice].append(student)
+    #Selecting the students according to the project's ranking order if they are currently match with this project
+    return [x for x in rankings[choice] if x in matchings[choice]]
+#be careful : inputs are modified 
+def stable_matching_algorithm_unbalanced_class(student_vows, rankings, places):
+    ##deep copy so as not to transform inputs !!
+    students =  {key: value[:] for key, value in student_vows.items()}
+    #initially all the students are waiting to be assign to a project
+    waiting_list = [applicants for applicants in students]
+    #initially all the projects are empty
+    matchings = {choice: [] for choice in places}
+
+    while waiting_list:
+        for applicant in waiting_list.copy():
+            if not students[applicant]:
+                waiting_list = remove_student_from_waiting_list(applicant,waiting_list)
+                continue
+            #each student propose to his first vow
+            choice = students[applicant].pop(0)
+            #If the project chosen by the student still has some available places :
+            if len(matchings[choice]) < places[choice]:
+                matchings[choice] = update_matchings(applicant, choice,matchings,rankings)
+                waiting_list = remove_student_from_waiting_list(applicant,waiting_list)
+            #If not we check if the project prefers the student to its current last member and 
+            # update the attributions according to the project's vows
+            else:
+                if rankings[choice].index(applicant) < rankings[choice].index(matchings[choice][-1]):
+                    matchings[choice] = update_matchings(applicant, choice,matchings,rankings)
+                    waiting_list = remove_student_from_waiting_list(applicant,waiting_list)
+                    waiting_list.append(matchings[choice].pop())
+    print(waiting_list)
+    return matchings
+
+def evaluate_satisfaction(students_vows,stable_matching_results):
+    student_POV = [[student, course] for student in students_vows for course, result in stable_matching_results.items() if student in result]
+    return [students_vows[student_POV [i][0]].index(student_POV [i][1]) for i in range(len(student_POV))]
+
